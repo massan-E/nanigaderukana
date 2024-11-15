9cb7356ca83dfb1011da9b34ce0dacdb530c0c46
massan-E <masahiro.greed@gmail.com>
2024-11-15 15:30:33 +0900

    remember_me機能完成

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,19 +5,18 @@ class UserSessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:session][:name])
     if @user && @user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
       reset_session
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       log_in @user
       redirect_to @user
     else
-      # エラーメッセージを作成する
       flash.now[:danger] = "Invalid email/password combination"
       render "new", status: :unprocessable_entity
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to login_path, status: :see_other
   end
 end
