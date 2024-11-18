ce8e94a2975429d40257fa2451ea8ca8bdd91c70
massan-E <masahiro.greed@gmail.com>
2024-11-18 19:22:40 +0900

    プルダウンでletterを送信できるように

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source "https://rubygems.org"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 8.0.0"
+gem "rails", "8.0.0"
 # The modern asset pipeline for Rails [https://github.com/rails/propshaft]
 gem "propshaft"
 # Use postgresql as the database for Active Record
@@ -41,6 +41,8 @@ gem "thruster", require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
+
+gem "ransack", "4.2.1"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,9 +1,12 @@
 class LettersController < ApplicationController
   before_action :set_letter, only: %i[ show edit update destroy ]
   before_action :set_letterbox, only: %i[ index show new create ]
+  before_action :set_program, only: %i[ index show new create ]
 
   def index
-    @letters = @letterbox.letters.all
+    @letters = @letterbox&.letters.all
+    # @q = Person.ransack(params[:q])
+    # @people = @q.result(distinct: true)
   end
 
   def show
@@ -12,6 +15,7 @@ class LettersController < ApplicationController
   def new
     @letter = Letter.new
     @letter.radio_name = current_user.name if current_user
+    @letter.letterbox_id = @letterbox&.id
   end
 
   def edit
@@ -19,7 +23,7 @@ class LettersController < ApplicationController
 
   def create
     @letter = @letterbox.letters.build(letter_params)
-    @letter.user_id = current_user.id if current_user
+    @letter.user_id = current_user&.id
     if @letter.save
       redirect_to letter_sent_path, notice: "Letter was successfully created."
     else
@@ -37,10 +41,7 @@ class LettersController < ApplicationController
 
   def destroy
     @letter.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to letters_path, status: :see_other, notice: "Letter was successfully destroyed." }
-    end
+    redirect_to letters_path, status: :see_other, notice: "Letter was successfully destroyed."
   end
 
   def sent; end
@@ -55,6 +56,12 @@ class LettersController < ApplicationController
     end
 
     def set_letterbox
-      @letterbox = Letterbox.find(params[:letterbox_id])
+      letterbox_id = params[:letter][:letterbox_id].to_i
+      @letterbox = Letterbox.find(letterbox_id) unless letterbox_id == 0
+    end
+
+    def set_program
+      program_id = params[:program_id]
+      @program = Program.find(program_id) if program_id
     end
 end
