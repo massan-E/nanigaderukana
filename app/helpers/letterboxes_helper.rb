3148edac6237e2ccabedcd804ba0c620c1a7fc69
massan-E <masahiro.greed@gmail.com>
2024-11-18 14:46:59 +0900

    前回のコミットの部分でミスがあったので修正

--- app/helpers/letterboxes_helper.rb
+++ app/helpers/letterboxes_helper.rb
@@ -2,6 +2,6 @@ module LetterboxesHelper
   include ProgramsHelper
 
   def letterbox_action_path(letterbox, program)
-    letterbox.present? ? letterbox_path(letterbox) : program_letterboxes_path(program)
+    letterbox.persisted? ? letterbox_path(letterbox) : program_letterboxes_path(program)
   end
 end
