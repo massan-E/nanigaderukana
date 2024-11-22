911dfcd77649c7f93a0a1c9efc1290cccfbd6206
massan-E <masahiro.greed@gmail.com>
2024-11-22 18:02:07 +0900

    letterbox編集エラー修正

--- app/helpers/letterboxes_helper.rb
+++ app/helpers/letterboxes_helper.rb
@@ -1,5 +1,5 @@
 module LetterboxesHelper
   def letterbox_action_path(program, letterbox)
-    letterbox.persisted? ? edit_program_letterbox_path(program, letterbox) : program_letterboxes_path(program, letterbox)
+    letterbox.persisted? ? program_letterbox_path(program, letterbox) : program_letterboxes_path(program, letterbox)
   end
 end
