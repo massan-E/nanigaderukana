33bd7c133ef6daea77a803510064039df9a7b556
massan-E <masahiro.greed@gmail.com>
2024-11-24 01:00:13 +0900

    いい方法思いついたので修正

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,6 +1,6 @@
 class StaticPageController < ApplicationController
   def top
-    @programs = Program.order(created_at: :desc).limit(3)
-    @letterboxes = Letterbox.order(created_at: :desc).limit(3)
+    @programs = Program.includes(:user).order(created_at: :desc).limit(3)
+    @letterboxes = Letterbox.includes(:program).order(created_at: :desc).limit(3)
   end
 end
--- app/helpers/programs_helper.rb
+++ app/helpers/programs_helper.rb
@@ -4,8 +4,4 @@ module ProgramsHelper
     produce = UserParticipation.find_by(user_id: user.id, program_id: program.id)
     produce.present?
   end
-
-  def invite_button
-    action_name == "show" ? "programs/invite_button" : "shared/nil"
-  end
 end
