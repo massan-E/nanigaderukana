24d563d8a93b3f9f5675f92fac25cc0b1c0cc9db
massan-E <masahiro.greed@gmail.com>
2024-11-28 00:23:46 +0900

    swiperをTOPペーじに実装＆i18n少し実装

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,6 +1,6 @@
 class StaticPageController < ApplicationController
   def top
-    @programs = Program.includes(:user).order(created_at: :desc).limit(3)
+    @programs = Program.includes(:user).order(created_at: :desc).limit(5)
     @letterboxes = Letterbox.includes(:program).order(created_at: :desc).limit(3)
   end
 end
