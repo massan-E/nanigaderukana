d93c4ce40850e7162cd039252d30d6aedc7a7fe5
massan-E <masahiro.greed@gmail.com>
2024-11-28 15:40:54 +0900

    swiperで表示するものを_programパーシャルにできるようにした（ヘルパー追加）

--- app/helpers/programs_helper.rb
+++ app/helpers/programs_helper.rb
@@ -4,4 +4,15 @@ module ProgramsHelper
     produce = UserParticipation.find_by(user_id: user.id, program_id: program.id)
     produce.present?
   end
+
+  def program_card_class
+    case action_name
+    when "index"
+      "col-md-6 col-lg-4"
+    when "top"
+      "h-100 w-100 py-4 px-4"
+    else
+      "col-md-6 col-lg-4"
+    end
+  end
 end
