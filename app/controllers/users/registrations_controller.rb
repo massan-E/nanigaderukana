7460cf157b2036f0d7741a6d30bc7460acae42b5
massan-E <masahiro.greed@gmail.com>
2024-11-30 03:48:27 +0900

    アカウント登録時にもemailを登録して確認メール送ってからemail有効化できるようになった

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,6 +11,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
+    # puts "パラムス　#{params.to_h}"
     build_resource(sign_up_params)  # Strong Parametersを使用して新しいユーザーを作成
 
     resource.skip_confirmation! # 確認をスキップ
@@ -18,8 +19,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
     if resource.save
       # ユーザーが保存された場合の処理
       sign_in(resource)  # サインインさせる
-      flash[:success] = "Wel come to Music Hour!!"
-      flash[:success] += "　ご登録いただいたメールアドレスに登録完了のお知らせを送信しました。" if resource.email.present?
+      flash[:notice] = "Wel come to Music Hour!!"
+      if params[:user][:email].present?
+        resource.unconfirmed_email = params[:user][:email]
+        resource.send_confirmation_instructions if resource.unconfirmed_email.present?
+        resource.save(validate: false)
+        flash[:notice] += "　メールアドレス宛てに確認メールを送信しました。メールに記載されているURLにアクセスし、メールアドレスを有効化してください" 
+      end
       respond_with resource, location: after_sign_up_path_for(resource)
     else
       clean_up_passwords resource
@@ -56,7 +62,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
