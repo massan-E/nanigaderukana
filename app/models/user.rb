2bfc65891758f7f5c3484f009236eadc40f29136
massan-E <masahiro.greed@gmail.com>
2024-12-01 04:27:13 +0900

    remeberme機能のエラー解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  attr_accessor :remember_token
-
   validates :email, uniqueness: true, allow_blank: true # deviceとともに追加
 
   validates :name,  presence: true, uniqueness: true, length: { maximum: 50 }
@@ -25,25 +23,6 @@ class User < ApplicationRecord
   # ランダムなトークンを返す
   def self.new_token
     SecureRandom.urlsafe_base64
-  end
-
-  def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
-    remember_digest
-  end
-
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
-
-  def forget
-    update_attribute(:remember_digest, nil)
-  end
-
-  def session_token
-    remember_digest || remember
   end
 
   # ransack用
