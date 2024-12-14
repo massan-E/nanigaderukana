a93f545603af3ad2eefd8c3d06f6c86dd7a11204
massan-E <masahiro.greed@gmail.com>
2024-12-14 17:03:53 +0900

    lint修正

--- config/initializers/rouge_theme.rb
+++ config/initializers/rouge_theme.rb
@@ -1,9 +1,9 @@
 # config/initializers/rouge_theme.rb
-require 'rouge'
-require 'fileutils'
+require "rouge"
+require "fileutils"
 
-rouge_theme = Rouge::Themes::MonokaiSublime.render(scope: '.highlight')
+rouge_theme = Rouge::Themes::MonokaiSublime.render(scope: ".highlight")
 
 # stylesheets ディレクトリにRougeのスタイルシートを作成
-FileUtils.mkdir_p(Rails.root.join('app', 'assets', 'stylesheets'))
-File.write(Rails.root.join('app', 'assets', 'stylesheets', '_rouge.scss'), rouge_theme)
+FileUtils.mkdir_p(Rails.root.join("app", "assets", "stylesheets"))
+File.write(Rails.root.join("app", "assets", "stylesheets", "_rouge.scss"), rouge_theme)
