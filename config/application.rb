358993756ac54ab4b9ba56e18736dcd99d835413
massan-E <masahiro.greed@gmail.com>
2025-04-03 18:21:24 +0900

    アプリへの独自ドメインの反映

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
-    config.hosts << "nanigaderukana.onrender.com"
+    config.hosts << "musichour.massan-e.com"
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
