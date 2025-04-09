61a30a39227caa2c0be719ba9b7417a8cdaafc30
massan-E <masahiro.greed@gmail.com>
2025-04-09 11:16:30 +0900

    検索機能等に対してprogramの非表示機能追加

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,6 +1,6 @@
 class StaticPageController < ApplicationController
   def top
-    @programs = Program.includes(:user).where(publish: true)order(created_at: :desc).limit(5)
+    @programs = Program.includes(:user).where(publish: true).order(created_at: :desc).limit(5)
     @letterboxes = Letterbox.includes(:program).where(publish: true).order(created_at: :desc).limit(3)
     @rankings = ProgramRanking.all.includes(:program)
   end
